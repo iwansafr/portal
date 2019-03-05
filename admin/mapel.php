a1bcdfb24122ccf6960adaae5f387a9b0df8e016
iwansafr <iwansafr@gmail.com>
2019-03-05 08:54:02 +0700

    edit title on mapel

--- admin/mapel.php
+++ admin/mapel.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Page Title</title>
+	<title>Atur Jadwal Mapel</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="../library/css/bootstrap.min.css" rel="stylesheet">
 	<script src="../library/js/jquery.min.js"></script>
