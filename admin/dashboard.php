0404f94a84e9b4710ee49ab35f65be9bc790e0f2
iwansafr <iwansafr@gmail.com>
2019-03-05 09:08:55 +0700

    home bugfix

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>dashboard</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Dashboard</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="../library/css/bootstrap.min.css" rel="stylesheet">
+	<script src="../library/js/jquery.min.js"></script>
+	<script src="../library/js/bootstrap.min.js"></script>
+	<script src="../library/datatables.net/js/jquery.dataTables.min.js"></script>
+	<script src="../library/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="container">
