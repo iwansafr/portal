58ab7926e84c34a0c23581e3978cb68ae4f4508c
iwansafr <iwansafr@gmail.com>
2019-03-05 09:04:36 +0700

    responsive menu

--- admin/config.php
+++ admin/config.php
@@ -1,33 +1,46 @@
-<link href="../library/css/bootstrap.min.css" rel="stylesheet">
-<div class="container">
-	<div class="row">
-		<?php
-		include 'menu.php';
-		include '../classes/config.php';
-		include '../classes/database.php';
-		include '../initial.php';
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Config</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../library/css/bootstrap.min.css" rel="stylesheet">
+	<script src="../library/js/jquery.min.js"></script>
+	<script src="../library/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<?php
+			include 'menu.php';
+			include '../classes/config.php';
+			include '../classes/database.php';
+			include '../initial.php';
 
-		$conf = new config($db);
-		$conf->save('main');
-		$data = $conf->get_config('main');
-		?>
-		<div class="col-md-3">
-			<div class="panel panel-default">
-				<div class="panel panel-heading">
-					Configuration
-				</div>
-				<div class="panel-body panel">
-					<form action="" method="post">
-						<div class="form-group">
-							<label for="judul">judul</label>
-							<input type="text" name="judul" class="form-control" placeholder="judul" value="<?php echo @$data['judul'] ?>">
-						</div>
-						<div class="form-group">
-							<button class="btn btn-sm btn-success">simpan</button>
-						</div>
-					</form>
+			$conf = new config($db);
+			$conf->save('main');
+			$data = $conf->get_config('main');
+			?>
+			<div class="col-md-3">
+				<div class="panel panel-default">
+					<div class="panel panel-heading">
+						Configuration
+					</div>
+					<div class="panel-body panel">
+						<form action="" method="post">
+							<div class="form-group">
+								<label for="judul">judul</label>
+								<input type="text" name="judul" class="form-control" placeholder="judul" value="<?php echo @$data['judul'] ?>">
+							</div>
+							<div class="form-group">
+								<button class="btn btn-sm btn-success">simpan</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</body>
+</html>
--- admin/menu.php
+++ admin/menu.php
@@ -1,14 +1,21 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu_main" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
       <a class="navbar-brand" href="index.php">Dashboard</a>
     </div>
-    <ul class="nav navbar-nav">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="user.php">User</a></li>
-      <li><a href="kelas.php">Kelas</a></li>
-      <li><a href="mapel.php">Mapel</a></li>
-      <li><a href="config.php">Config</a></li>
-    </ul>
+    <div class="collapse navbar-collapse" id="menu_main">
+      <ul class="nav navbar-nav">
+        <li><a href="user.php">User</a></li>
+        <li><a href="kelas.php">Kelas</a></li>
+        <li><a href="mapel.php">Mapel</a></li>
+        <li><a href="config.php">Config</a></li>
+      </ul>
+    </div>
   </div>
 </nav>
