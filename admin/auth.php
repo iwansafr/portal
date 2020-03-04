4c424b9d8fa7d2b4bdeb8052cb017834c3ee1446
iwansafr <iwansafr@gmail.com>
2020-03-04 19:13:55 +0700

    only echo if failed login

--- admin/auth.php
+++ admin/auth.php
@@ -10,12 +10,11 @@ $user = new User($db);
 $auth = $user->login($username,$password);
 if(!empty($auth))
 {
-	echo @$auth['msg'];
 	if(!empty($auth['status']))
 	{
-		print_r($_SESSION);
-		print_r($auth);
 		$_SESSION['user'] = $auth['data'];
 		header('location: index.php');
+	}else{
+		echo @$auth['msg'];
 	}
 }
